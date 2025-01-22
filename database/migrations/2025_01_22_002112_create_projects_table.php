2eb2c6fe1addbdc217fbd6d3aebf0acc28355a3d
kaiconima <riveannugraha04@smp.belajar.id>
2025-01-22 08:47:59 +0700

    praktikum migrate

---
+++ database/migrations/2025_01_22_002112_create_projects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('project_id');
+            $table->string('project_name');
+            $table->date('project_date');
+            $table->string('author');
+            $table->text('desc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
